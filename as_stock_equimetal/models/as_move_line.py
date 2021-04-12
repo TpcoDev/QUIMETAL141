91f85533878902185ad132bbad635046c79cbc72
phoenixsampras <phoenixsampras@gmail.com>
2021-04-12 05:43:02 -0400

    QUIMETAL 8.0

--- as_stock_equimetal/models/as_move_line.py
+++ as_stock_equimetal/models/as_move_line.py
@@ -15,7 +15,10 @@ class StockMoveLine(models.Model):
         return codigo
 
     def as_barcode_2(self):
-        codigo = '(37)'+str(self.product_id.as_cantidad_unidades)+'(3101)'+str(self.qty_done)+'(15)'+str(self.expiration_date.strftime('%d%m%Y'))
+        fecha_vencimiento = ''
+        if self.lot_id.expiration_date:
+            fecha_vencimiento = self.lot_id.expiration_date.strftime('%d%m%Y')
+        codigo = '(37)'+str(self.product_id.as_cantidad_unidades)+'(3101)'+str(self.qty_done)+'(15)'+str(fecha_vencimiento)
         return codigo
 
 class StockMoveLine(models.Model):
@@ -30,14 +33,14 @@ class StockMoveLine(models.Model):
     def as_barcode_1(self):
         fecha_vencimiento = ''
         for line in self.move_line_ids:
-            if line.expiration_date:
-                fecha_vencimiento = line.expiration_date.strftime('%d%m%Y')
+            if line.lot_id.expiration_date:
+                fecha_vencimiento = line.lot_id.expiration_date.strftime('%d%m%Y')
         codigo = '(10)'+str(self.as_get_date_lote())+'(91)'+str(self.product_id.default_code)+'(37)'+str(self.product_id.as_cantidad_unidades)+'(3101)'+str(self.quantity_done)+'(11)'+str(fecha_vencimiento)
         return codigo
 
     def as_fecha_vencimiento(self):
         fecha_vencimiento = ''
         for line in self.move_line_ids:
-            if line.expiration_date:
-                fecha_vencimiento = line.expiration_date.strftime('%d%m%Y')
+            if line.lot_id.expiration_date:
+                fecha_vencimiento = line.lot_id.expiration_date.strftime('%d%m%Y')
         return fecha_vencimiento
