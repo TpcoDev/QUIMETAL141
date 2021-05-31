72e8ed12a985ab4052d19b9f6b3767b31b409248
phoenixsampras <phoenixsampras@gmail.com>
2021-05-31 19:52:46 -0400

    QUIMETAL 49.0

--- as_equimetal_barcode/__manifest__.py
+++ as_equimetal_barcode/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : "Ahorasoft EQUIMETAL Barcode",
-    'version' : "1.0.4",
+    'version' : "1.0.5",
     'author'  : "Ahorasoft",
     'description': """
 Webservice dummy equimetal
