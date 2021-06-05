df7b3d0042d14548429cd1e969b9c397b1f0076d
phoenixsampras <phoenixsampras@gmail.com>
2021-06-05 09:48:15 -0400

    QUIMETAL 57.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-1changelog
+changelog
 02/04/2021 [as_stock_equimetal] Wizard para etiquetas de MP.
--- as_equimetal_barcode/__manifest__.py
+++ as_equimetal_barcode/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : "Ahorasoft EQUIMETAL Barcode",
-    'version' : "1.0.6",
+    'version' : "1.0.7",
     'author'  : "Ahorasoft",
     'description': """
 Webservice dummy equimetal
