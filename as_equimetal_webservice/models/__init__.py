d3ce02f3fd2013f0635e5b446b9649d383b9d511
wmoreno <walter.moreno@tpco.cl>
2021-08-23 17:19:18 -0400

    Trazabilidad

---
+++ as_equimetal_webservice/models/__init__.py
@@ -0,0 +1,6 @@
+from . import as_mesage_log
+from . import as_stock_picking
+from . import as_res_config
+from . import as_stock_picking_type
+from . import as_sale_order
+from . import as_purchase_order
